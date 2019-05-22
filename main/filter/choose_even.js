503a4eba9c36ff874d1d94f1d87822cb9217203c
linhaishe <xusumu@gmail.com>
2019-05-23 00:05:50 +0800

    filter

--- main/filter/choose_even.js
+++ main/filter/choose_even.js
@@ -1,8 +1,11 @@
 'use strict';
 
 function choose_even(collection) {
-
-  //在这里写入代码
+  return collection.filter(v =>v%2===0);
 }
+//var n = [];
+//n.push(collection.filter(choose_even));
 
+//return n;
+//how can i type in es5?????????????
 module.exports = choose_even;
