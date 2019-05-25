de7b5a1f1370d5182ee8d0bc94b26af519e0947c
linhaishe <xusumu@gmail.com>
2019-05-25 23:58:00 +0800

    update

--- main/flaten/double_dimensional_array_to_one_1.js
+++ main/flaten/double_dimensional_array_to_one_1.js
@@ -1,8 +1,22 @@
 'use strict';
 
 function double_to_one(collection) {
-
-  //在这里写入代码
+  // var flattened = [[0, 1], [2, 3], [4, 5]].reduce(
+  //   function(a, b) {
+  //     return a.concat(b);
+  //   },
+  //   []
+  // );
+  // flattened is [0, 1, 2, 3, 4, 5]
+  var collection = [1, [2], [3, 4]].reduce(
+    function(a,b){
+      return a.concat(b);
+    },
+    []
+  );
+  console.log(collection)
+  return collection;
 }
+//能把我这个方法在简化一下
 
 module.exports = double_to_one;
--- main/flaten/double_dimensional_array_to_one_2.js
+++ main/flaten/double_dimensional_array_to_one_2.js
@@ -1,8 +1,13 @@
 'use strict';
 
 function double_to_one(collection) {
-
-  //在这里写入代码
+  var collection = [[1, 2, 3], [5, 2, 1, 4], [2, 1]].reduce(
+    function(a,b){
+      return a.concat(b);
+    },
+    []
+  );
+  return Array.from(new Set(collection));
 }
 
 module.exports = double_to_one;
