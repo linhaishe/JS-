9b5d52ebea912a2b5f7f2d7a05475027d6f93c38
xusumu <xusumu@gmail.com>
2019-05-28 17:46:36 +0800

    update

--- main/reduce/collect_max_number.js
+++ main/reduce/collect_max_number.js
@@ -1,7 +1,14 @@
 'use strict';
 
 function collect_max_number(collection) {
-  //在这里写入代码
+  //let result = Math.max(1, 2, 3, 4, 5);
+  // let result = collection.reduce(function(x,y){
+  //   return x>y?x:y;
+  // });
+  let result = Math.max(...collection);
+  return result;
 }
+
+
 
 module.exports = collect_max_number;
--- main/reduce/collect_min_number.js
+++ main/reduce/collect_min_number.js
@@ -1,7 +1,11 @@
 'use strict';
 
 function collect_min_number(collection) {
-  //在这里写入代码
+  // let result = collection.reduce(function(x,y){
+  //   return x<y?x:y;
+  // })
+  let result = Math.min(...collection);
+  return result;
 }
 
 module.exports = collect_min_number;
