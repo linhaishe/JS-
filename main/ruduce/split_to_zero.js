4b52afba4e8848f01ce445081d85dbd750808f55
linhaishe <xusumu@gmail.com>
2019-05-28 22:40:39 +0800

    ruduce

--- main/ruduce/split_to_zero.js
+++ main/ruduce/split_to_zero.js
@@ -1,7 +1,13 @@
 'use strict';
 
 function spilt_to_zero(number, interval) {
-  //在这里写入代码
+  //?????????????
+  let result = [];
+  for(let num = number;parseFloat((num+interval).toFixed(1))>0;num-=interval ){
+      result.push(parseFloat(num.toFixed(1)));
+  }
+  console.log(result);
+  return result;
 }
 
 module.exports = spilt_to_zero;
