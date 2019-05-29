ae1343c5f1c8eb32ab8dcb4a1fa3b6d739787b05
xusumu <xusumu@gmail.com>
2019-05-29 18:10:51 +0800

    update

--- main/section_2/practice_2/practice.js
+++ main/section_2/practice_2/practice.js
@@ -1,5 +1,38 @@
 function count_same_elements(collection) {
-  //在这里写入代码
+  let result = [];
+  let obj = {};
+  let b = -1;
+  for(let item of collection ){
+    if(item.length > 1){
+      let ch = item[0];
+      let count = item[2];
+
+      if(!obj[ch]){
+        obj[ch] = {} ;
+        obj[ch].key = ch ;
+        obj[ch].count = parseInt(count);
+      }else{
+        obj[ch].count += parseInt(count) 
+      } 
+    }
+    else{
+      if(!obj[item]){
+        obj[item] = {} ;
+        obj[item].key = item ;
+        obj[item].count = 1;
+      }
+      else{
+        obj[item].count ++;   
+      } 
+    }
+   
+  }
+  for(let item in obj)
+  {
+    result.push(obj[item]);
+  }
+  console.log(result);
+  return result;
 }
 
 module.exports = count_same_elements;
