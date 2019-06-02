fd8a98c8f7ddbe2dc4f262c9eac653fd0572e743
linhaishe <xusumu@gmail.com>
2019-06-02 10:02:58 +0800

    update

--- main/section_1/practice_4/practice.js
+++ main/section_1/practice_4/practice.js
@@ -4,23 +4,26 @@ function collect_same_elements(collection_a, object_b) {
   let result = [];
   for(let i  = 0;i<collection_a.length;i++){
     for (let j in collection_a[i]){
-      arr1.push(collection_a[i][j]);
-      //????????????????????????????????????a[i][j]
-    }
-  }
-  console.log(arr1);
- 
-  for(let m in object_b){
-    arr2 = object_b[m];
-  }
-  console.log(arr2);
-  for(let i = 0;i<arr1.length;i++){
-    for(let j = 0;j<arr2.length;j++){
-      if(arr1[i] === arr2[j]){
-        result.push(arr1[i]);
+      for(let k in object_b.value){
+        if(collection_a[i][j] === object_b.value[k]){
+          result.push(collection_a[i][j]);
+        }
       }
+      //一个对象有多个键值，
     }
   }
+  // console.log(arr1);
+  // for(let m in object_b){
+  //   arr2 = object_b[m];
+  // }
+  // console.log(arr2);
+  // for(let i = 0;i<arr1.length;i++){
+  //   for(let j = 0;j<arr2.length;j++){
+  //     if(arr1[i] === arr2[j]){
+  //       result.push(arr1[i]);
+  //     }
+  //   }
+  // }
   console.log(result);
   return result;
 }
--- main/section_2/practice_1/practice.js
+++ main/section_2/practice_1/practice.js
@@ -4,14 +4,17 @@ function count_same_elements(collection) {
   let answer = [];
    for(let item of collection){
        if(!obj[item]){
+         //obj[item] undefined
           obj[item] = {};
           obj[item].key = item;
           obj[item].count = 1;
+          //设置对象的键值
        }
        else{
           obj[item].count++;
        }
    }
+   console.log(obj);
    for (let key in obj){
        answer.push(obj[key]);
    }
